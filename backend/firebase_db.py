ae93126844cbee9c215bf01b8937069bb7bf1da6
sruthi <asruthi3525@gmail.com>
2026-06-14 13:22:54 +0530

    added firebase

---
+++ backend/firebase_db.py
@@ -0,0 +1,15 @@
+import firebase_admin
+
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate(
+    "serviceAccountKey.json"
+)
+
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+print("Firebase Connected Successfully")
